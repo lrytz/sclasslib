b2e2faa02b4246fa6dc63ca6f25803fba3ce017f
Tarfaa <tarfaa.m@gmail.com>
2011-01-06 16:42:39 +0100

    some fixes

--- src/org/gjt/jclasslib/browser/config/ScalaConfig.scala
+++ src/org/gjt/jclasslib/browser/config/ScalaConfig.scala
@@ -4,10 +4,8 @@ import classpath.{ClasspathEntry, ClasspathChangeEvent, ClasspathChangeListener}
 import org.gjt.jclasslib.browser._
 import tools.nsc.{Settings, Global}
 import org.gjt.jclasslib.structures.elementvalues.ScalaSigDetailElementValue
-import tools.nsc.symtab.Symbols
 import java.io.File
 import org.gjt.jclasslib.io.Log
-
 /**
  * 
  * @author Lomig Mégard
@@ -18,16 +16,16 @@ object ScalaConfig extends ClasspathChangeListener {
   // hack to get this instance from Java
   def instance = this
 
-  var global: Global = _
-  var config: BrowserConfig = _
+  private var global: SclasslibGlobal = _
+  private var config: BrowserConfig = _
 
   /*
    * map of nodes that are waiting for an updated classpath
    */
   private var markedNodes: Map[String, BrowserTreeNode] = Map()
-  
+
   def setConfig(c: BrowserConfig): Unit = config = c
-  
+
   def classpathChanged(event: ClasspathChangeEvent) = updateGlobal()
 
   def updateGlobal() {
@@ -35,7 +33,7 @@ object ScalaConfig extends ClasspathChangeListener {
     val settings = new Settings()
     settings.processArgumentString(cp)
 
-    val g = new Global(settings)
+    val g = new SclasslibGlobal(settings)
     val run = new g.Run()
     global = g
 
@@ -48,40 +46,56 @@ object ScalaConfig extends ClasspathChangeListener {
   def insertSignatureDetails(className: String): Unit = {
     markedNodes.get(className) match {
       case Some(parent) =>
-        fillTree(className.replace(File.separatorChar, '.'), parent)
+        global.fillTree(className.replace(File.separatorChar, '.'), parent)
         markedNodes -= className
       case None =>
-        Log.error("Try to insert nodes in " + className + ". But this class had not been marked.")
+        Log.error("Try to insert nodes in " + className + ", but this class had not been marked.")
     }
   }
 
-  private def getClassSymbol(name: String) = {
-    val g = global
-    val typeName = g.newTermName(name)
-    val sym = g.definitions.getClass(typeName)
 
-    sym
-  }
 
-  private def fillTree(className: String, parent: BrowserTreeNode): Unit = {
-    val classSym = getClassSymbol(className)
-    fillTree(classSym, 1, parent)
-  }
-
-  private def fillTree(sym: Symbols#Symbol, index: Int, parent: BrowserTreeNode): Unit = {
-    val entryNode =
-      new BrowserTreeNode("[" + index + "] " + sym.nameString,
-        BrowserTreeNode.NODE_SCALASIG,
-        index,
-        new ScalaSigDetailElementValue(sym))
-    parent.add(entryNode)
-
-    if (sym.isClass || sym.isModule || sym.isModuleClass) {
-      val members = sym.info.decls.toList
-      for ((memberSym, i) <- members.zipWithIndex) {
-        fillTree(memberSym, i, entryNode)
+  private class SclasslibGlobal(settings: Settings) extends Global(settings) {
+    object typeInitializer extends TypeTraverser {
+      def traverse(tp: Type) {
+        mapOver(tp)
+      }
+      override def mapOver(origSyms: List[Symbol]): List[Symbol] = {
+        origSyms.foreach(_.initialize)
+        origSyms
       }
     }
+
+    def getClassSymbol(name: String) = {
+      val typeName = newTermName(name)
+      val sym = definitions.getClass(typeName)
+      sym
+    }
+
+    def fillTree(className: String, parent: BrowserTreeNode): Unit = {
+      val classSym = getClassSymbol(className)
+      fillTree(classSym, 1, parent)
+    }
+
+    def fillTree(sym: Symbol, index: Int, parent: BrowserTreeNode): Unit = {
+      typeInitializer(sym.info)
+      val entryNode =
+        new BrowserTreeNode("[" + index + "] " + sym.nameString,
+          BrowserTreeNode.NODE_SCALASIG,
+          index,
+          new ScalaSigDetailElementValue(sym))
+      parent.add(entryNode)
+
+      if (sym.isClass || sym.isModule || sym.isModuleClass) {
+        val members = sym.info.decls.toList
+        for ((memberSym, i) <- members.zipWithIndex) {
+          //g.typeInitializer(memberSym.info)
+          fillTree(memberSym, i, entryNode)
+        }
+      }
+    }
+
   }
+
 
 }
